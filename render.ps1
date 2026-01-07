ca71b28fad9d1fd58c39b7bef78136c89d03c767
mariam <mariem.hamrouni@polytechnicien.tn>
2026-01-07 11:39:48 +0100

    Built site for gh-pages

--- render.ps1
+++
@@ -1,4 +0,0 @@
-Set-StrictMode -Version Latest
-cd rapport
-quarto render
-Write-Host "✅ Site généré dans: rapport/_site"
--- render.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd rapport
-quarto render
-echo "✅ Site généré dans: rapport/_site"
